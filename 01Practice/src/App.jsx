ddbb274354eae5237a06edc9968f345dba890391
mansi <mansi@gmail.com>
2026-03-02 12:36:52 +0530

    01 ka test

---
+++ 01Practice/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const[counter, setCounter] = useState(0)
+
+  function add(){
+    if(counter < 20){
+      setCounter(counter +1)
+    }
+  }
+  function remove(){
+    if(counter > 0){
+      setCounter(counter -1)
+    }                                
+  }
+function reset(){
+  setCounter(0)
+}
+
+
+  return (
+    <>
+    <h1>counter value  :{counter}</h1>
+    <p>my age : {counter}</p>
+      <button onClick={() => add()} >Increment</button> {""}
+      <button onClick={() => remove()} >Decrement</button> 
+      <br />
+      <br />
+      <button onClick={()=> reset()}>reset</button>
+      <p>let's start counting :{counter}</p>
+    </>
+  )
+}
+
+export default App
