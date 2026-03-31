814246d79f43e2a3368777048dd55252656f47ae
mansi <yashbudakoti@gmail.com>
2026-03-31 17:47:12 +0530

    custom hook

---
+++ currencyConvertor/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1 className='text-3xl text-center bg-orange-500' >Currency App</h1>
+    </>
+  )
+}
+
+export default App
