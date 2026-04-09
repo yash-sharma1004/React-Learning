8c8290133dbadb287784f4fafa0d35ddf62cae7e
mansi <yashbudakoti@gmail.com>
2026-04-09 12:31:30 +0530

    useMemo

--- ReactRouter/src/main.jsx
+++ ReactRouter/src/main.jsx
@@ -1,7 +1,7 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+
 import './index.css'
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import Layout from './Layout.jsx'
